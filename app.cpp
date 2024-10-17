aa1eaa5c8a4248c5d6243967e623ec5d59576259
AmrElsherbiny <amoryelsherbiny@gmail.com>
2024-10-17 12:40:36 +0300

    operation on 2 var

--- app.cpp
+++ app.cpp
@@ -3,13 +3,20 @@ using namespace std ;
 
 int main (){
 
-   cout<<"Hello";
+  //  cout<<"Hello";
 
+    // operation on 2 var
+   int x ; 
+   int y ;
+   int total ;
 
+   cout<<"Hello\n please write 2 numbers aftereach one click enter ";
 
-
-
-
+   cin>> x ; 
+   cin>> y ;
+   total = x+y ;
+   cout<< total ;
+    // end
 
 
     return 0 ;
