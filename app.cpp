0a96442eab1f2dbbe751d260772f020b217e1b90
AmrElsherbiny <amoryelsherbiny@gmail.com>
2024-10-17 14:23:27 +0300

    swap variables value

--- app.cpp
+++ app.cpp
@@ -6,17 +6,31 @@ int main (){
   //  cout<<"Hello";
 
     // operation on 2 var
-   int x ; 
-   int y ;
-   int total ;
+//    int x ; 
+//    int y ;
+//    int total ;
 
-   cout<<"Hello\n please write 2 numbers aftereach one click enter \n";
+//    cout<<"Hello\n please write 2 numbers aftereach one click enter \n";
 
-   cin>> x ; 
-   cin>> y ;
-   total = x+y ;
-   cout<< "Total = " << total ;
+//    cin>> x ; 
+//    cin>> y ;
+//    total = x+y ;
+//    cout<< "Total = " << total ;
     // end
+
+
+// swap variables value start
+
+int a = 5 ; 
+int b = 15 ; 
+int box = a ; 
+
+a = b ; 
+b = box ; 
+
+cout << b ; 
+// swap variables value end 
+
 
 
     return 0 ;
